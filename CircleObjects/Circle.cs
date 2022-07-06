a861240c74191fa37eb04dd94e6893e2a6e1ccd2
Krista <knderson21@gmail.com>
2022-07-05 22:29:39 -0400

    finished circle objects lab

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    internal class Circle
+    {
+        //properties
+        public double Radius;
+
+
+
+        //constructor
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+
+
+        //methods
+        public double CalculateCircumference()
+        {
+            double circumference = 2 * Math.PI * Radius;
+            return circumference;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Math.PI * (Radius * Radius);
+            return area;
+        }
+
+        public string CalculateFormattedCircumference(double x)
+        {
+            double roundedx = Math.Round(x, 2);
+            return FormatNumber(roundedx);
+        }
+
+        public string CalculateFormattedArea(double y)
+        {
+            double roundedY = Math.Round(y, 2);
+            return FormatNumber(roundedY);
+        }
+
+        private string FormatNumber(double x)
+        {
+            return x.ToString();
+        }
+
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using CircleObjects;
+
+double radius = 0;
+
+int i = 0;
+
+bool tryAgain = true;
+
+Console.WriteLine("Welcome to the Circle Tester!");
+
+while (tryAgain)
+{
+    i++;
+    Console.WriteLine("Enter radius: ");
+
+    while (true)
+    {
+        while (!double.TryParse(Console.ReadLine(), out radius))
+        {
+            Console.WriteLine("That was not a number, try again:");
+        }
+
+        //Create the new circle
+        Circle newCircle = new Circle(radius);
+
+        double x = newCircle.CalculateCircumference();
+        Console.WriteLine("Circumference: " + newCircle.CalculateFormattedCircumference(x));
+
+        double y = newCircle.CalculateArea();
+        Console.WriteLine("Radius:" + newCircle.CalculateFormattedArea(y));
+        break;
+    }
+
+    Console.WriteLine("Continue? y/n: ");
+    string restart = Console.ReadLine().ToLower();
+
+    if (restart != "y")
+    {
+        tryAgain = false;
+    }
+
+    
+}
+Console.WriteLine($"\nGoodbye! You created {i} Cirle Objects.");
+
